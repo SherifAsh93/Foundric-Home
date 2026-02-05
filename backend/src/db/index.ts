5f234bef9a42e76fa7025119a7c4a2d1d5edea74
Sherif <sherif.ash93@gmail.com>
2026-02-05 21:20:21 +0200

    cconfirm

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 if (!process.env.DB_URL) {
   throw new Error("DB_URL is not defined");
 }
-// if okay, initialize PstgreSql connection pool
+// if okay, initialize PostgreSql connection pool
 const pool = new Pool({ connectionString: process.env.DB_URL });
 
 pool.on("connect", () => {
